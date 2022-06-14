10b58f7bbb8209f2b3f850be260abcaf3c4caff9
Karina <ruth.macedo@ucsp.edu.pe>
2022-06-13 20:33:41 -0500

    dowland eslint, prettier, develop front end LogIn, Validate imputs email and password, implementation of style and images

--- bq-project/src/app/app.module.ts
+++ bq-project/src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- bq-project/src/app/login/login.component.ts
+++ bq-project/src/app/login/login.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  loginPerson() {
+    console.log(this.loginForm);
+    class User {
+      email: string;
+      password: string;
+      constructor(email: string, password: string){
+        this.email = email;
+        this.password = password;
+      }
+    }
+    const USER: User = {
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value,
+    }
+    console.log(USER);
   }
 
 }
