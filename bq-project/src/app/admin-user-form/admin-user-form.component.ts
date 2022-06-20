8096bdf2d3f4432db442b262837b2e580bdb74c4
Karina <ruth.macedo@ucsp.edu.pe>
2022-06-20 10:46:30 -0500

    forms

--- bq-project/src/app/admin-user-form/admin-user-form.component.ts
+++ bq-project/src/app/admin-user-form/admin-user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-user-form',
@@ -6,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-user-form.component.scss'],
 })
 export class AdminUserFormComponent implements OnInit {
-  constructor() {}
+  userForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      id: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {}
+
+  // addPerson() {
+  //   console.log(this.userForm);
+  //   class Users {
+  //     id: number;
+  //     email: string;
+  //     password: string;
+  //     roles: {
+  //       admin: boolean;
+  //     }
+  //     constructor(id:number, email:string, password: string, roles: {admin: boolean}) {
+  //       this.id = id;
+  //       this.email = email;
+  //       this.password = password;
+  //       this.roles = roles;
+  //     }
+  //   }
+  //   const USERS: Users = {
+  //     id: this.id.get('id')?.value,
+  //     email: this.userForm.get('email')?.value,
+  //     password: this.userForm.get('password')?.value,
+  //     roles: this.userForm.get('roles')?.value,
+  //   };
+  //   console.log(USERS);
+  // }
 }
