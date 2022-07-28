fb4ca5e054545776081fcab0402d1f1d1292b715
Karina <ruth.macedo@ucsp.edu.pe>
2022-07-27 22:55:46 -0500

    Fix CSS problems

--- src/app/admin/admin-product/admin-products-form/admin-products-form.component.ts
+++ src/app/admin/admin-product/admin-products-form/admin-products-form.component.ts
@@ -10,21 +10,25 @@ import { ResourceLoader } from '@angular/compiler';
   styleUrls: ['./admin-products-form.component.scss'],
 })
 export class AdminProductsFormComponent implements OnInit {
-
   productForm: FormGroup;
   title = 'Agregar Producto';
-  productID !:number;
+  productID!: number;
   dataOrder: any = new Date();
 
-  constructor(private fb: FormBuilder, private bdproductService:  BdProductService, private toastr: ToastrService,private bdproductsService:  BdProductService) {
+  constructor(
+    private fb: FormBuilder,
+    private bdproductService: BdProductService,
+    private toastr: ToastrService,
+    private bdproductsService: BdProductService
+  ) {
     // Obtiene los valores del formulario ProductForm
     this.productForm = this.fb.group({
       name: ['', Validators.required],
       price: ['', Validators.required],
       image: ['', Validators.required],
       type: ['', Validators.required],
-      dateEntry: [ this.dataOrder],
-    })
+      dateEntry: [this.dataOrder],
+    });
   }
 
   ngOnInit(): void {
@@ -33,12 +37,12 @@ export class AdminProductsFormComponent implements OnInit {
   }
   //Obtiene el id del disparador de admin productlist
   obtainIdProduct() {
-    this.bdproductService.disparador.subscribe(data=>{
-      return this.productID = data.dataProduct.id;
-    })
+    this.bdproductService.disparador.subscribe(data => {
+      return (this.productID = data.dataProduct.id);
+    });
   }
   //Agrega o edita productos
-  addProduct(){
+  addProduct() {
     const PRODUCTS: productSinId = {
       name: this.productForm.get('name')?.value,
       price: this.productForm.get('price')?.value,
@@ -46,43 +50,60 @@ export class AdminProductsFormComponent implements OnInit {
       type: this.productForm.get('type')?.value,
       dateEntry: this.productForm.get('dateEntry')?.value,
     };
-    if(this.productID!== undefined) {
+    if (this.productID !== undefined) {
       //editar Producto de
-      this.bdproductService.editBdProductService(this.productID, PRODUCTS).subscribe(data => {
-        this.toastr.success('El producto fue actualizado con éxito', 'Producto Actualizado');
-        this.productForm.reset();
-        this.bdproductsService.update.emit({
-          update:true
-        });
-      },error => {console.log(error)})
-    } else{
+      this.bdproductService
+        .editBdProductService(this.productID, PRODUCTS)
+        .subscribe(
+          data => {
+            this.toastr.success(
+              'El producto fue actualizado con éxito',
+              'Producto Actualizado'
+            );
+            this.productForm.reset();
+            this.bdproductsService.update.emit({
+              update: true,
+            });
+          },
+          error => {
+            console.log(error);
+          }
+        );
+    } else {
       // Crear producto, emitir mensaje
-      this.bdproductService.postBdProductService(PRODUCTS).subscribe(data => {
-        this.toastr.success('El producto fue agregado con éxito', 'Producto Actualizado');
-        this.productForm.reset();
-        this.bdproductsService.update.emit({
-          update:true
-        });
-      },error => {console.log(error)}
-      )
+      this.bdproductService.postBdProductService(PRODUCTS).subscribe(
+        data => {
+          this.toastr.success(
+            'El producto fue agregado con éxito',
+            'Producto Actualizado'
+          );
+          this.productForm.reset();
+          this.bdproductsService.update.emit({
+            update: true,
+          });
+        },
+        error => {
+          console.log(error);
+        }
+      );
     }
     // window.location.reload();
   }
-// Obtiene los valores del servidor los muestra en el formulario y permite su reasignación
+  // Obtiene los valores del servidor los muestra en el formulario y permite su reasignación
   editFormProduct() {
-    this.bdproductService.disparador.subscribe(data => { //datos obtenidos por el disparador desde product-list
-      if(data.dataProduct.id !== null){
-      this.title = 'Editar Producto';
-      this.productForm.setValue({ // Reasignación de valores
-        name: data.dataProduct.name,
-        price: data.dataProduct.price,
-        image: data.dataProduct.image,
-        type: data.dataProduct.type,
-        dateEntry: this.dataOrder,
-      })
-    }
-  })
+    this.bdproductService.disparador.subscribe(data => {
+      //datos obtenidos por el disparador desde product-list
+      if (data.dataProduct.id !== null) {
+        this.title = 'Editar Producto';
+        this.productForm.setValue({
+          // Reasignación de valores
+          name: data.dataProduct.name,
+          price: data.dataProduct.price,
+          image: data.dataProduct.image,
+          type: data.dataProduct.type,
+          dateEntry: this.dataOrder,
+        });
+      }
+    });
+  }
 }
-}
-
-
--- src/app/admin/admin-user/admin-user-form/admin-user-form.component.ts
+++ src/app/admin/admin-user/admin-user-form/admin-user-form.component.ts
@@ -7,13 +7,17 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-admin-user-form',
   templateUrl: './admin-user-form.component.html',
-  styleUrls: ['./admin-user-form.component.scss']
+  styleUrls: ['./admin-user-form.component.scss'],
 })
 export class AdminUserFormComponent implements OnInit {
   userForm: FormGroup;
   title = 'Agregar Usuario';
-  userID !:number;
-  constructor(private fb: FormBuilder,private bduserService: BdUserService, private toastr: ToastrService) {
+  userID!: number;
+  constructor(
+    private fb: FormBuilder,
+    private bduserService: BdUserService,
+    private toastr: ToastrService
+  ) {
     //Get the values ​​using the formBuilder method
     this.userForm = this.fb.group({
       email: ['', Validators.required],
@@ -28,8 +32,8 @@ export class AdminUserFormComponent implements OnInit {
   //Get the id of the admin user list
   obtainId() {
     this.bduserService.disparador.subscribe(data => {
-      return this.userID = data.dataUser.id;
-    })
+      return (this.userID = data.dataUser.id);
+    });
   }
   //Edit person and create persona, create object user
   addPerson() {
@@ -38,43 +42,63 @@ export class AdminUserFormComponent implements OnInit {
       password: this.userForm.get('password')?.value,
       roles: {
         description: this.userForm.get('roles')?.value,
-        admin: this.userForm.get('roles')?.value=== 'admin'? true:false,
-      }
+        admin: this.userForm.get('roles')?.value === 'admin' ? true : false,
+      },
     };
-    if(this.userID!== undefined) {
+    if (this.userID !== undefined) {
       //Edit product
-      this.bduserService.editBdUserService(this.userID,USERS).subscribe(data => {
-        this.toastr.success('El usuario fue actualizado con éxito', 'Usuario Actualizado');
-        this.userForm.reset();
-        this.bduserService.update.emit({
-          update:true
-        });
-      },error => {console.log(error)}
-      )
+      this.bduserService.editBdUserService(this.userID, USERS).subscribe(
+        data => {
+          this.toastr.success(
+            'El usuario fue actualizado con éxito',
+            'Usuario Actualizado'
+          );
+          this.userForm.reset();
+          this.bduserService.update.emit({
+            update: true,
+          });
+        },
+        error => {
+          console.log(error);
+        }
+      );
     } else {
       //Creat product if id is defined
-      this.bduserService.postBdUserService(USERS).subscribe(data => {
-        this.toastr.success('El usuario fue agregado con éxito', 'Usuario Agregado');
-        this.userForm.reset();
-        this.bduserService.update.emit({
-          update:true
-        });
-      },error => {console.log(error)})
+      this.bduserService.postBdUserService(USERS).subscribe(
+        data => {
+          this.toastr.success(
+            'El usuario fue agregado con éxito',
+            'Usuario Agregado'
+          );
+          this.userForm.reset();
+          this.bduserService.update.emit({
+            update: true,
+          });
+        },
+        error => {
+          console.log(error);
+        }
+      );
     }
     // window.location.reload();
   }
   //Get data from userListComponent and reassign values
   editForm() {
     this.bduserService.disparador.subscribe(data => {
-      console.log('Recibiendo Data:', data)
-      if(data.dataUser.id !== null) {
+      console.log('Recibiendo Data:', data);
+      if (data.dataUser.id !== null) {
         this.title = 'Editar Usuario';
         this.userForm.setValue({
           email: data.dataUser.email,
           password: '',
-          roles: data.dataUser.roles.description ==='admin'?'admin':data.dataUser.roles.description ==='weiter'?'weiter':'chef'
-        })
+          roles:
+            data.dataUser.roles.description === 'admin'
+              ? 'admin'
+              : data.dataUser.roles.description === 'weiter'
+              ? 'weiter'
+              : 'chef',
+        });
       }
-    })
+    });
   }
 }
